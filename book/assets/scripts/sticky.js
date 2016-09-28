3997d09449a243e830c82d1a151fddc514f2e137
Subil <subil1407@gmail.com>
2016-09-28 22:05:45 +0530

    the adding viewport script is being uncooperative because of rouge bytes here and there. I'm leaving the webpages be. Removing the clojure book

--- book/assets/scripts/sticky.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  // $("#top-nav").append($(".topnav-content"))
-  // $("#top-nav").sticky()
-  $(".secondary .wrapper").sticky({
-    topSpacing: 15,
-    getWidthFrom: ".secondary"
-  });
-});
--- book/rename_html.py
+++
@@ -1,7 +0,0 @@
-import os
-
-filename_list = os.listdir()
-filename_list = [filename for filename in filename_list if os.path.isfile(filename)]
-for filename in filename_list:
-    if not filename.find('.') > -1:
-        os.rename(filename,"{}.html".format(filename))
